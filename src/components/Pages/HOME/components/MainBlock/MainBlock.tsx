aed069f3afff8bbd91179991c19a680e3159098a
Sergey Otinov <otinov.prog@gmail.com>
2025-11-11 19:24:17 +0200

    change

--- src/components/Pages/HOME/components/MainBlock/MainBlock.tsx
+++ src/components/Pages/HOME/components/MainBlock/MainBlock.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { CSSProperties, FC, useEffect, useState } from "react";
 import css from "./MainBlock.module.css";
 import { UI } from "@/components/UI";
 import Content from "@/content/en.json" assert { type: "json" };
@@ -7,10 +7,11 @@ import right_block_css from "./RightBlock.module.css";
 import clsx from "clsx";
 import Image from "next/image";
 import { motion } from "framer-motion";
-// import { useMediaQuery } from "react-responsive";
+import { useMediaQuery } from "react-responsive";
 
 export const MainBlock: FC = () => {
     const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+    const isDesktop = useMediaQuery({ query: "(min-width: 1430px)" });
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -25,33 +26,53 @@ export const MainBlock: FC = () => {
     const ap = {
         initial: {
             opacity: 0,
-            transform: "rotateX(0deg) rotateY(0deg)",
+            transform: "rotateX(0deg) rotateY(0deg) translateX(-16px)",
         },
         animate: {
             opacity: 1,
-            transform: "rotateX(0deg) rotateY(-1deg)",
+            transform: `rotateX(${isDesktop ? 0 : -2}deg) rotateY(${isDesktop ? -1 : -3}deg) translateX(-16px)`,
         },
     };
+
+    const mobileBtnStyle = {
+        // position: "absolute",
+        // padding: "14px 24px",
+        // zIndex: 3,
+        // left: "24px",
+        // bottom: "0",
+        // width: "fit-content",
+        // height: "fit-content",
+        // border: "1px solid #fff",
+    } as CSSProperties;
 
     return (
         <section className={css.main_block}>
             <div className={css.slider}>
-                <div className={css.left}>
-                    <div className={clsx(css.content_block, isVideoLoaded ? css.animate : "")}>
-                        <div className={css.curtain} />
-                        <h1>{Content.Home.h1}</h1>
-                        <p>{Content.Home.description}</p>
-                        <UI.Button variant="link" colorScheme="tertiary" href="#request-form" style={{ padding: "14px 24px" }}>
-                            {Content.Home.button}
-                        </UI.Button>
-                        <div className={css.dark_curtain} />
+                {isDesktop && (
+                    <div className={css.left}>
+                        <div className={clsx(css.content_block, isVideoLoaded ? css.animate : "")}>
+                            <div className={css.curtain} />
+                            <h1>{Content.Home.h1}</h1>
+                            <p>{Content.Home.description}</p>
+                            <UI.Button variant="link" colorScheme="tertiary" href="#request-form" style={{ padding: "14px 24px" }}>
+                                {Content.Home.button}
+                            </UI.Button>
+                            <div className={css.dark_curtain} />
+                        </div>
                     </div>
-                </div>
+                )}
                 <div className={right_block_css.right_block} style={{ borderColor: isVideoLoaded ? "#fff" : "#ffffff" }}>
                     {!isVideoLoaded ? (
                         <Image src="/images/svg/loader.svg" width={50} height={50} alt="loader" />
                     ) : (
                         <motion.div initial={ap.initial} animate={ap.animate} transition={{ duration: 0.7, delay: 0.2 }} className={right_block_css.video_block}>
+                            {!isDesktop && (
+                                <div className={css.btn_wrapper}>
+                                    <UI.Button variant="link" colorScheme="tertiary" href="#request-form" style={mobileBtnStyle}>
+                                        {Content.Home.button}
+                                    </UI.Button>
+                                </div>
+                            )}
                             <ReactPlayer className={right_block_css.video} muted={true} loop autoPlay={true} src="/video/main-block-1.mp4" />
                         </motion.div>
                     )}
