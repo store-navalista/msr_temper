de06bbf383297640d23b88a0a1fecd359a720044
Sergey Otinov <otinov.prog@gmail.com>
2025-12-05 20:09:02 +0200

    fix

--- src/app/api/news/current-new/[id]/route.ts
+++ src/app/api/news/current-new/[id]/route.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: Promise<{ id: number }> }) {
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
 
     if (!id) {
